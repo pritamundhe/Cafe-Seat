eee5216cb3438648479b115f501b6f07298f2ced
Pritam Mundhe <pritammundhe00@gmail.com>
2025-09-25 11:22:54 +0530

    Initial commit

---
+++ src/main/java/com/cafe/CafeSeat/CafeSeatApplication.java
@@ -0,0 +1,13 @@
+package com.cafe.CafeSeat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeSeatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafeSeatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cafe/CafeSeat/CafeSeatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cafe.CafeSeat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeSeatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
